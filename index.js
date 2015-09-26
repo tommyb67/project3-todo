24cf732a1c05e3be87fb1777f52a3b52070d5d76
Tom Brennan <tommybrennan@gmail.com>
2015-09-26 11:05:17 -0400

    fully function to do list just need it to persist

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ $(document).ready(function () {
       event.preventDefault();
 
       var item = $("#todo-list-item").val();
+
+      if (item) {
+        $("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr /></li>");
+        $("#todo-list-item").val("");
+      }
+    });
+
+    $(document).on("change", ".checkbox", function() {
+      $(this).parent().toggleClass("completed");
+    });
+
+    $(document).on("click", ".remove", function() {
+      $(this).parent().remove();
     });
 });
